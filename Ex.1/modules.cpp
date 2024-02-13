fd3d4c0a02c652774f5523a359418ccf0abe65fe
whostephan <129996786+whostephan@users.noreply.github.com>
2024-02-13 16:30:39 +0300

    Add files via upload

---
+++ Ex.1/modules.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "utils.h"
+using namespace std;
+
+int main()
+{
+    int x = 3, y = 5;
+    cout << x + y << endl;
+    return Plus(x, y);
+}
+
+
---
+++ Ex.1/utils.h
@@ -0,0 +1,2 @@
+#pragma once
+int Plus(int a, int b);
